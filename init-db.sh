081a64488bcf7f575ce0f617e141cd9d444a73f2
Benjamin Idewor <benjamin.idewor@momox.biz>
2025-02-26 17:21:04 +0100

    Update Readme and Startup instructions

--- init-db.sh
+++ init-db.sh
@@ -4,9 +4,11 @@ set -e
 # Function to create users and databases
 setup_db() {
   local database=$1
-  echo "Creating database '$database'"
-  psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" <<-EOSQL
-    CREATE DATABASE $database;
+  echo "Creating database '$database' if it does not exist..."
+  psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname=postgres <<-EOSQL
+    SELECT 'CREATE DATABASE $database'
+    WHERE NOT EXISTS (SELECT FROM pg_database WHERE datname = '$database')
+    \gexec
     GRANT ALL PRIVILEGES ON DATABASE $database TO $POSTGRES_USER;
 EOSQL
 }
@@ -17,5 +19,5 @@ if [ -n "$POSTGRES_MULTIPLE_DATABASES" ]; then
   for db in $(echo $POSTGRES_MULTIPLE_DATABASES | tr ',' ' '); do
     setup_db $db
   done
-  echo "Multiple databases created"
+  echo "Multiple databases created."
 fi
